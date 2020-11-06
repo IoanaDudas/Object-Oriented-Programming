ebc7e82a3e863dffacd48c862a6fc43ef36f5e90
IoanaDudas <72069893+IoanaDudas@users.noreply.github.com>
2020-11-06 13:22:04 +0200

    All oop projects

--- Shopping List/cmake-build-debug/CMakeFiles/ShoppingList_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ShoppingList_autogen"
-  "ShoppingList_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ShoppingList_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
